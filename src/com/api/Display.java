5bc628099de0647572fffd0367b58a17cb29a2a5
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2017-06-21 10:29:43 +0300

    Init commit

---
+++ src/com/api/Display.java
@@ -0,0 +1,14 @@
+package com.api;
+
+// TODO: Displays can be:
+// 1. Monochrome and color
+// 2. LCD and LED
+// 3. TV, computer, and portable device
+
+// Can you apply the Bridge pattern here?
+public interface Display {
+
+    void turnOn();
+
+    void turnOff();
+}
